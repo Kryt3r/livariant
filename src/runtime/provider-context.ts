c90105c8c62204f9a640bf8891b6da8d20cff09e
Vaeltharis <shadow.kryter@gmail.com>
2026-08-13 22:56:34 +0200

    Expose provider context runtime

---
+++ src/runtime/provider-context.ts
@@ -0,0 +1,12 @@
+export { buildProviderContext } from "./provider-context-build.js";
+export type { ProviderContextBuildOptions } from "./provider-context-build.js";
+export { readProviderContextTaskFile, PROVIDER_CONTEXT_TASK_MAX_BYTES } from "./provider-context-task.js";
+export type {
+  BlockedProviderContextPacket,
+  ProviderContextEvidence,
+  ProviderContextPacket,
+  ProviderContextProjection,
+  ProviderContextProvider,
+  ProviderContextTask,
+  ReadyProviderContextPacket,
+} from "./provider-context-types.js";
