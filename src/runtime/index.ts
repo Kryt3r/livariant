c6acbf2317b6fbc93da55490d824c2a4d04e94cb
Vaeltharis <shadow.kryter@gmail.com>
2026-08-18 09:28:58 +0200

    Expose protected semantic proposal builder

--- src/runtime/index.ts
+++ src/runtime/index.ts
@@ -2,6 +2,7 @@ export * from "./index-core.js";
 export { runProtectedDoctor as runDoctor } from "./protected-doctor.js";
 export { buildProtectedProjectContextSnapshot as buildProjectContextSnapshot } from "./protected-context.js";
 export { buildProtectedResumeContext as buildResumeContext } from "./protected-resume.js";
+export { buildProtectedSemanticProposal as buildSemanticProposal } from "./protected-semantic-proposal.js";
 export * from "./drift-assessment.js";
 export * from "./provider-context.js";
 export * from "./semantic-maintenance.js";
