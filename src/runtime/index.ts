c2d5c48b85bb823506ea20440653674301a396f2
Vaeltharis <shadow.kryter@gmail.com>
2026-08-18 11:47:59 +0200

    Expose protected Runtime lifecycle wrappers

--- src/runtime/index.ts
+++ src/runtime/index.ts
@@ -3,6 +3,7 @@ export { runProtectedDoctor as runDoctor } from "./protected-doctor.js";
 export { buildProtectedProjectContextSnapshot as buildProjectContextSnapshot } from "./protected-context.js";
 export { buildProtectedResumeContext as buildResumeContext } from "./protected-resume.js";
 export { buildProtectedSemanticProposal as buildSemanticProposal } from "./protected-semantic-proposal.js";
+export { applyProtectedNormalUpdate as applyNormalUpdate, applyProtectedMigrationUpdate as applyMigrationUpdate } from "./protected-lifecycle.js";
 export * from "./drift-assessment.js";
 export * from "./provider-context.js";
 export * from "./semantic-maintenance.js";
