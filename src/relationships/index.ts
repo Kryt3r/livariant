f9e5c3745147f4ee2e31c185da056e15c4c6dbf4
Vaeltharis <shadow.kryter@gmail.com>
2026-08-19 16:31:56 +0200

    WP-036: export relationship module

---
+++ src/relationships/index.ts
@@ -0,0 +1,11 @@
+export {
+  STABLE_RELATIONSHIP_SCHEMA_VERSION,
+  createStableRelationship,
+  stableRelationshipIdentity,
+  validateRelationshipEntityReference,
+  validateStableRelationship,
+  type RelationshipEntityKind,
+  type RelationshipEntityReference,
+  type StableRelationship,
+  type StableRelationshipType,
+} from "./relationship-contract.js";
