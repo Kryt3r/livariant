70e91b0c0ae38da50ea2f869e151ee42dcbd4a16
Vaeltharis <shadow.kryter@gmail.com>
2026-09-13 13:16:01 +0200

    WP-055: register operator broadcast trust pipeline

--- apps/desktop/src-tauri/src/lib.rs
+++ apps/desktop/src-tauri/src/lib.rs
@@ -9,6 +9,7 @@ mod github_telemetry;
 mod notification_center;
 mod notification_product_events;
 mod operator_broadcast;
+mod operator_broadcast_pipeline;
 mod operator_broadcast_state;
 mod operator_broadcast_transport;
 mod operator_broadcast_verify;
