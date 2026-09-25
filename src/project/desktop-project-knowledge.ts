52948cc4dd79da79ab3780c5294327098ce7fabd
Vaeltharis <shadow.kryter@gmail.com>
2026-09-25 17:59:34 +0200

    WP-056: import stderr for concise Project Knowledge host errors

--- src/project/desktop-project-knowledge.ts
+++ src/project/desktop-project-knowledge.ts
@@ -1,6 +1,6 @@
 import { readFile } from "node:fs/promises";
 import { resolve } from "node:path";
-import { stdin, stdout } from "node:process";
+import { stderr, stdin, stdout } from "node:process";
 import { discoverProject } from "./discovery.js";
 import { ensureDesktopProjectBrainStorage } from "./desktop-project-brain-storage.js";
 import { ProjectBrainStore } from "../project-brain/store.js";
